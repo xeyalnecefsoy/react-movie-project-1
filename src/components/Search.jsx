780a4d1a1e413a56606e437259b987693712648a
Khayal Najafsoy <xeyalnecefsoy@gmail.com>
2025-02-05 17:09:14 +0400

    Layihə Github hesabıma yükləndi

---
+++ src/components/Search.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+
+
+const Search = ({ searchTerm, setSearchTerm }) => {
+
+  return (
+    <div className='search'>
+      <div>
+        <img src="search.svg" alt="axtarış" />
+
+        <input
+          type="text"
+          placeholder="Filmlər axtar"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+
+      </div>
+    </div>
+  )
+}
+
+export default Search
